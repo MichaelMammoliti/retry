815232fde6088ad99ec7ff3fe02ce1eda6804dd3
Michael Mammoliti <mammoliti.michael@gmail.com>
2023-09-22 09:58:07 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const retry = (fn, { 
+  maxAttemts = 0,
+  delay = 1000, 
+  increaseDelayBy = 1000, 
+  increaseDelayAfter = 10,
+  onRetry = () => {},
+}) => {
+  let counter = 0;
+  let currentDelay = delay;
+
+  const attempt = async (...args) => {
+    try {
+      return fn(...args);
+    } catch (error) {
+      if (counter < maxAttemts) {
+        if (counter >= increaseDelayAfter) {
+          currentDelay += increaseDelayBy;
+        }
+
+        if (!delay) {
+          onRetry();
+          attempt(...args);
+
+          return;
+        } else {
+          setTimeout(() => {
+            onRetry();
+            attempt(...args);
+          }, currentDelay);
+        }
+      } else {
+        throw error;
+      }
+    } finally {
+      counter += 1;
+    }
+  };
+
+  return attempt;
+};
